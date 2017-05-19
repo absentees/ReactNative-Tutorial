fedcb5c3f04e8f59220629db689d3923791182d6
absentees <scottblissett@gmail.com>
2017-05-19 18:40:49 +1000

    😎

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,32 @@
 // Import libraries for making components
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 
 // Make a components
 const Header = () => {
-	return <Text></Text>;
+	const { textStyle, viewStyle } = styles;
+
+	return (
+		<View style={viewStyle}>
+			<Text style={textStyle}>Albums!</Text>
+		</View>
+	);
+};
+
+const styles = {
+	viewStyle: {
+		backgroundColor: '#f8f8f8',
+		justifyContent: 'center',
+		alignItems: 'center',
+		height: 60,
+		paddingTop: 15,
+		shadowColor: '#000',
+		shadowOffset: { width: 0, height: 2 },
+		shadowOpacity: 0.2
+	},
+	textStyle: {
+		fontSize: 20
+	}
 };
 
 // Make the component available to other parts of the app
