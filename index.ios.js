70d30532760d5d8e09c1135376cd1640bdeceae9
absentees <scottblissett@gmail.com>
2017-06-21 17:29:51 +1000

    finished taylor swift app.

--- index.ios.js
+++ index.ios.js
@@ -1,11 +1,16 @@
 // Import library to help create a Component
 import React from 'react';
-import { AppRegistry } from 'react-native';
-import Header from './src/components/header';
+import { AppRegistry, View } from 'react-native';
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
+
 
 // Create a Component
 const App = () => (
-	<Header />
+	<View style={{ flex: 1 }}>
+		<Header headerText={'Albums'} />
+		<AlbumList />
+	</View>
 );
 
 // Render to device
